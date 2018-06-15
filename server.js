0ae93acff6e18dff9afd96245bac53a3d02623a8
pazuzu111 <pazuzublue111@gmail.com>
2018-06-15 04:37:23 -0400

    cleaned up server

--- server.js
+++ server.js
@@ -5,33 +5,41 @@ const socketIo = require("socket.io");
 const axios = require("axios");
 const port = process.env.PORT || 4001;
 const router = express.Router();
+const server = http.createServer(app)
+const io = socketIo(server)
 
 router.get("/", (req, res) => {
-  res.send({ response: "I am alive" }).status(200);
+  res.send({ response: "I am alive" }).status(200)
 });
 
-const server = http.createServer(app);
-const io = socketIo(server);
+let interval;
 
+//open connection
 io.on("connection", socket => {
-  console.log("New client connected"), setInterval(
-    () => getApiAndEmit(socket),
-    10000
-  )
-  socket.on("disconnect", () => console.log("Client disconnected"))
+  console.log("New client connected")
+
+  //if there is an interval running then clear
+  if(interval) {clearInterval(interval)}
+
+  interval = setInterval(() => getApiAndEmit(socket), 10000)
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected")
+  })
 })
 
+//fetch api
 const getApiAndEmit = async socket => {
   try {
-    const res = await axios.get(
-      "https://api.coingecko.com/api/v3/coins?per_page=10"
-    )
+    const res = await axios.get("https://api.coingecko.com/api/v3/coins?per_page=10")
 
+    //send data to client
     socket.emit("FromAPI", res.data)
     console.log(res.data)
   } catch(error) {
-    console.error(`Error: ${error.code}`);
+    console.error(`Error: ${error.code}`)
   }
 }
 
+//listening(open)
 server.listen(port, () => console.log(`Listening on port ${port}`))
